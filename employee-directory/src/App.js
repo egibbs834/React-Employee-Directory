8dcda94d34b5e2aa8a8683050fc277e83e1284a5
egibbs834 <edwardgibbons34@gmail.com>
2020-09-20 22:41:47 -0400

    working app

---
+++ employee-directory/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "./components/Main/Main.js";
+import Wrapper from "./components/Wrapper/Wrapper.js";
+import Header from "./components/Header/Header.js";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
